1a76b0ef5350e51dc9b3cc5815daae54e85f40f4
Matthew Forrester <matthew.forrester@speechmarks.com>
2019-02-10 12:18:24 +0000

    progress

--- five/src/main.rs
+++ five/src/main.rs
@@ -1,19 +1,15 @@
+use std::collections::HashMap;
 use std::env;
 use std::str::FromStr;
-use std::collections::HashMap;
-
 
 type Table = HashMap<String, Vec<u8>>;
-
 
 enum Input {
     S(String),
     I(u8),
 }
 
-
-fn to_input(s: &String) -> Input {
-    // Input::S(s.to_string())
+fn convert_to_input(s: &String) -> Input {
     match u8::from_str(s) {
         Ok(n) => Input::I(n),
         Err(_e) => Input::S(s.to_string())
@@ -21,12 +17,26 @@ fn to_input(s: &String) -> Input {
 }
 
 
-fn to_table(tab: &mut Table, input: Input) -> &mut Table {
-    let k: String = "a".to_string();
-    match tab.get(&k.to_string()) {
-        Some(mut v) => v.append(0),
-        None => {
-            tab.insert(k.to_string(), vec![0]);
+struct TableBuilder {
+    table: Table,
+    active_key: String,
+}
+
+impl TableBuilder {
+    fn get_active_key(&self) -> String {
+        self.active_key.to_string()
+    }
+}
+
+
+fn to_table_builder(mut tab: TableBuilder, input: Input) -> TableBuilder {
+    let k = TableBuilder::get_active_key(&tab);
+    match input {
+        Input::S(s) => { tab.active_key = s }
+        Input::I(n) => {
+            if k != "_".to_string() {
+            tab.table.entry(k).and_modify(|v| v.push(n)).or_insert(vec![n]);
+            }
         }
     }
     tab
@@ -34,14 +44,34 @@ fn to_table(tab: &mut Table, input: Input) -> &mut Table {
 
 
 fn print_table(tab: &Table) {
-
+    let mut first = true;
+    for (k, vs) in tab {
+        let mut pre = "\n\n".to_string();
+        if first { pre = "".to_string(); }
+        first = false;
+        println!("{}{}", pre, k);
+        for v in vs {
+            println!(" * {}", v);
+        }
+    }
+    println!("\n");
 }
 
-
 fn main() {
+
     let args: Vec<String> = env::args().collect();
 
-    let inputs: Vec<Input> = args[1..].into_iter().map(to_input).collect();
+    let tab = TableBuilder {
+        table: HashMap::new(),
+        active_key: "_".to_string(),
+    };
 
+    let table = args[1..].into_iter()
+        .map(convert_to_input)
+        .fold(tab, to_table_builder)
+        .table;
     // println!("Sum: {:?}", inputs);
+
+    print_table(&table);
+
 }
